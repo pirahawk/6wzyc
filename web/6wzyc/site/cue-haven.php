dc6d78cb40dac26121aad83d181e439f1eea954b
Piran Tata <piran.tata@gmail.com>
2014-04-20 15:42:04 +1200

    #7 Added missing cue haven page

---
+++ web/6wzyc/site/cue-haven.php
@@ -0,0 +1,33 @@
+<?php
+    require '_layout.php';
+?>
+
+<?php
+    function renderBody(){
+?>
+
+<div class="slide">
+    <div class="container text-left">
+        <div class="row">
+            <h1><strong>CUE Haven</strong></h1>
+            <p class="lead">
+                CUE Haven is an initiative run by Thomas and Mahrukh Stazyk that is committed to the preservation of native plant and animal life. The 60 acre farm in Araparera, an hour north of the city and overlooking the Kaipara Harbour is to be reconverted into native forest. The restoration plan will be completed over a 15 year period. The undertaking is meticulously planned far into its completion and the Stazyks work painstakingly hard in adhering to its each stage. The detailed re-vegetation attempts to create a haven among all too common stretches of agricultural land being divided and sub-divided for lifestyle blocks or property development.
+            </p>
+
+            <p class="lead">
+                The parallel between the objectives of the congress and that of the cause is drawn in the need for collective effort. Our congress will stress the necessity to evolve as a faith and a community. Like the trees being planted, we only secure perpetuity through adaptability once our roots take to foreign ground. The Stazyks are admirably dedicated to preserving this land’s legacy just as the congress is in preserving that of our religion.
+            </p>
+
+            <p class="lead">
+                Thomas and Mahrukh Stazyk carry out their vision with an emphatic and contagious zeal that rings true with all that the youth congress stands for in its aim of preserving posterity. We’ve chosen nature to be a central fixture of the congress just as Zoroastrianism is predominantly a faith founded on the transcendence of nature. CUE Haven is more than a unique cause; it’s an imperative in a world that too readily overlooks nature. The Zoroastrian Youth of New Zealand will plant trees on the behalf of congress attendees in an attempt to offset their carbon footprint. In time, these plants will grow into a lasting and collective contribution.
+            </p>
+        </div>
+
+    </div>
+</div>
+
+
+<?php
+    }    
+?>
+
