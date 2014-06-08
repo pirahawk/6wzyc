4de242d85d46f54046ddbd4a59378936783290ef
Piran Tata <piran.tata@gmail.com>
2014-06-08 18:25:10 +1200

    #16 Added a new contact us Page form

---
+++ web/6wzyc/site/contact-us.php
@@ -0,0 +1,30 @@
+<?php
+    require '_layout.php';
+    
+    function renderBody(){
+?>
+
+<div class="slide">
+    <div class="container">
+        <div class="row">
+            <h1><strong>Contact Us</strong></h1>
+            <address>
+                <strong>6<sup>th</sup> World Zoroastrian Youth Congress</strong><br />
+                <i class="fa fa-envelope fa-6"></i> <span>&#054;&#116;&#104;&#119;&#122;&#121;&#099;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;</span>
+            </address>
+            
+            <address>
+                <strong>Social Media</strong><br />
+                <a href="https://twitter.com/6WZYC" target="_blank"><i class="fa fa-twitter-square fa-6"></i></a>
+                <a href="https://www.facebook.com/groups/223424211030360/" target="_blank"><i class="fa fa-facebook-square fa-6"></i></a>
+            </address>
+        </div>
+    </div>
+</div>
+
+<?php
+    
+        renderFooter();
+    }
+    
+?>
