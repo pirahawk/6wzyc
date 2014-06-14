4ce2b482d8b133c103bde8c6f8f3f7405b07c6a6
Piran Tata <piran.tata@gmail.com>
2014-06-14 23:08:47 +1200

    Swapped images on accommodation page

--- web/6wzyc/site/accommodation.php
+++ web/6wzyc/site/accommodation.php
@@ -10,20 +10,13 @@
             array("kc-2.jpg"),
             array("kc-3.jpg"),
             array("kc-4.jpg"),
-            array("kc-5.jpg"),
-            array("kc-6.jpg"),
-            array("kc-7.jpg"),
-            array("kc-8.jpg"),
-            array("kc-9.jpg"),
-            array("kc-10.jpg"),
-            array("kc-11.jpg"),
-            array("kc-12.jpg"));
+            array("kc-5.jpg"));
     
             foreach($images as $img){
 ?>
 
 <div class="item <?php echo $img[1] ?>">
-    <img src="<?php echo createImagePath($img[0],"location")?>" alt="<?php echo $img[0] ?>" />
+    <img src="<?php echo createImagePath($img[0],"location")?>" alt="<?php echo $img[0] ?>" class="location-img" />
 </div>
 <?php
             }    
@@ -32,23 +25,10 @@
     
     function renderBody(){
 ?>
-<div class="slide">
-    <div class="container text-left">
-        <div class="row">
-            <h1><strong>King's College</strong></h1>
-            <div class="carousel col-lg-offset-2" data-ride="carousel" data-interval="3000">
-                <div class="carousel-inner">
-                    <?php renderCarousel() ?>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
 <div class="slide green">
     <div class="container text-left">
         <div class="row">
+            <h1><strong>King's College</strong></h1>
 
             <p class="lead">
                 Founded in 1896, King's College is one of New Zealand's top private secondary schools. It prides itself at being the best it can be through the College’s various Disciplines, which includes: academic, sporting, cultural, spiritual and social. With links to the Anglican Church, the foundation of Kings is built on generosity of spirit, gratitude, honesty, moral courage, pursuit of learning, respect, spirituality and tolerance (http://www.kingscollege.school.nz).
@@ -56,10 +36,17 @@
             <p class="lead">
                 The 6thWZYC team have grown to create a highly respectable and professional working relationship with Kings College and are very confident that Kings will be a great host to our delegates. We cannot wait to show off this little gem situated in the suburbs of Auckland and we hope to see you all there!
             </p>
+
+
         </div>
     </div>
 </div>
 
+<div class="carousel col-lg-offset-2" data-ride="carousel" data-interval="3000">
+    <div class="carousel-inner">
+        <?php renderCarousel() ?>
+    </div>
+</div>
 
 <div class="slide black">
     <div class="container text-left">
