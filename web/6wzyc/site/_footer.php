63ab75e1d2551640a3b38b861d9777e17f68c197
Piran Tata <piran.tata@gmail.com>
2014-08-06 23:29:03 +1200

    Adjusting for extra height caused by sponsor logos in footer

--- web/6wzyc/site/_footer.php
+++ web/6wzyc/site/_footer.php
@@ -2,21 +2,21 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <div class="center-txt">
+                <div class="sponsor-container">
                     <a href="http://www.seruminstitute.com/" target="_blank"><img src="<?php echo createImagePath("poona.jpg","sponsors")?>" alt="sponsor" /></a>
                 </div>
             </div>
         </div>
 
-         <div class="row">
+        <div class="row">
             <div class="col-md-6">
-                <div class="center-txt">
+                <div class="sponsor-container">
                     <img src="<?php echo createImagePath("hk.jpg","sponsors")?>" alt="sponsor" />
                 </div>
             </div>
             <div class="col-md-6">
-                <div  class="center-txt">
-                    <h3 class="special-footer-ad">Mr Rusy M. Shroff, <em>BBS MBE</em> <br/> Mrs Purviz R.Shroff</h3>
+                <div class="center-txt">
+                    <h3 class="special-footer-ad">Mr Rusy M. Shroff, <em>BBS MBE</em> <br /> Mrs Purviz R.Shroff</h3>
                 </div>
             </div>
         </div>
