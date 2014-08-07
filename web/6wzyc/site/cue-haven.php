e7f749381e00446c3133bafa987c4d074abab02d
Piran Tata <piran.tata@gmail.com>
2014-08-07 23:53:28 +1200

    Cue Haven page converted to new design

--- web/6wzyc/site/cue-haven.php
+++ web/6wzyc/site/cue-haven.php
@@ -1,9 +1,6 @@
 <?php
     require '_layout.php';
-?>
-
-
-<?php
+    
     function renderCarousel(){
     
             $images = array(
@@ -21,44 +18,45 @@
             }    
     }    
     
-    
     function renderBody(){
 ?>
 
-<div class="jumbotron slide">
+<div class="page-title">
     <h1>CUE Haven</h1>
 </div>
 
-<div class="slide">
+<section class="black">
     <div class="container">
+        <h1>About <em>Cue</em> Haven</h1>
         <div class="row">
-            <div class="carousel col-lg-offset-2" data-ride="carousel" data-interval="3000" data-pause="none">
-                <div class="carousel-inner">
-                    <?php renderCarousel() ?>
-                </div>
+            <div class="col-12">
+                <p>
+                CUE Haven is an initiative run by Thomas and Mahrukh Stazyk that is committed to the preservation of native plant and animal life. The 60 acre farm in Araparera, an hour north of the city and overlooking the Kaipara Harbour is to be reconverted into native forest. The restoration plan will be completed over a 15 year period. The undertaking is meticulously planned far into its completion and the Stazyks work painstakingly hard in adhering to its each stage. The detailed re-vegetation attempts to create a haven among all too common stretches of agricultural land being divided and sub-divided for lifestyle blocks or property development.
+                </p>
+
+                <p>
+                The parallel between the objectives of the congress and that of the cause is drawn in the need for collective effort. Our congress will stress the necessity to evolve as a faith and a community. Like the trees being planted, we only secure perpetuity through adaptability once our roots take to foreign ground. The Stazyks are admirably dedicated to preserving this land’s legacy just as the congress is in preserving that of our religion.
+                </p>
+
+                <p>
+                Thomas and Mahrukh Stazyk carry out their vision with an emphatic and contagious zeal that rings true with all that the youth congress stands for in its aim of preserving posterity. We’ve chosen nature to be a central fixture of the congress just as Zoroastrianism is predominantly a faith founded on the transcendence of nature. CUE Haven is more than a unique cause; it’s an imperative in a world that too readily overlooks nature. The Zoroastrian Youth of New Zealand  plant a tree for every individual that donates to the cause - including delegates travelling to our shores, in order to help offset their carbon footprint. In time, these plants will grow into a lasting and collective contribution.
+                </p>
+                <a class="btn btn-primary btn-lg" href="http://cuehaven.com/" target="_blank">Find Out More <i class="fa fa-angle-right"></i> </a>
             </div>
         </div>
     </div>
-</div>
+</section>
 
-<div class="slide green">
-    <div class="container text-left">
-        <div class="row">
-            <p class="lead">
-                CUE Haven is an initiative run by Thomas and Mahrukh Stazyk that is committed to the preservation of native plant and animal life. The 60 acre farm in Araparera, an hour north of the city and overlooking the Kaipara Harbour is to be reconverted into native forest. The restoration plan will be completed over a 15 year period. The undertaking is meticulously planned far into its completion and the Stazyks work painstakingly hard in adhering to its each stage. The detailed re-vegetation attempts to create a haven among all too common stretches of agricultural land being divided and sub-divided for lifestyle blocks or property development.
-            </p>
 
-            <p class="lead">
-                The parallel between the objectives of the congress and that of the cause is drawn in the need for collective effort. Our congress will stress the necessity to evolve as a faith and a community. Like the trees being planted, we only secure perpetuity through adaptability once our roots take to foreign ground. The Stazyks are admirably dedicated to preserving this land’s legacy just as the congress is in preserving that of our religion.
-            </p>
-
-            <p class="lead">
-                Thomas and Mahrukh Stazyk carry out their vision with an emphatic and contagious zeal that rings true with all that the youth congress stands for in its aim of preserving posterity. We’ve chosen nature to be a central fixture of the congress just as Zoroastrianism is predominantly a faith founded on the transcendence of nature. CUE Haven is more than a unique cause; it’s an imperative in a world that too readily overlooks nature. The Zoroastrian Youth of New Zealand  plant a tree for every individual that donates to the cause - including delegates travelling to our shores, in order to help offset their carbon footprint. In time, these plants will grow into a lasting and collective contribution.
-            </p>
-            <a class="btn btn-primary btn-lg" href="http://cuehaven.com/" target="_blank">Find Out More <i class="fa fa-angle-right"></i> </a>
+<section>
+    <div class="container">
+        <div class="carousel" data-ride="carousel" data-interval="3000" data-pause="none">
+            <div class="carousel-inner">
+                <?php renderCarousel() ?>
+            </div>
         </div>
     </div>
-</div>
+</section>
 
 <?php
     renderFooter();
