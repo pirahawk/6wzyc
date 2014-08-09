a87a17607666e1714f7ea18960a17fa74c218b71
Piran Tata <piran.tata@gmail.com>
2014-08-09 12:51:13 +1200

    Contact us page restyled

--- web/6wzyc/site/contact-us.php
+++ web/6wzyc/site/contact-us.php
@@ -4,28 +4,35 @@
     function renderBody(){
 ?>
 
-<div class="jumbotron slide">
+<div class="page-title">
     <h1>Contact Us</h1>
 </div>
 
-<div class="slide">
+<section>
     <div class="container">
-        <div class="row">
-            <h1><strong>Our Mailing address is:</strong></h1>
-            <address>
-                <strong>6<sup>th</sup> World Zoroastrian Youth Congress</strong><br />
-                <?php renderPostalAddress()?>
-                <i class="fa fa-envelope fa-6"></i> <?php renderEmailAddress()?>
-            </address>
-
-            <address>
-                <strong>Social Media</strong><br />
-                <a href="https://twitter.com/6WZYC" target="_blank"><i class="fa fa-twitter-square fa-6"></i></a>
-                <a href="https://www.facebook.com/groups/223424211030360/" target="_blank"><i class="fa fa-facebook-square fa-6"></i></a>
-            </address>
-        </div>
+        <h1>Our Mailing address</h1>
+        <address>
+            <strong>6<sup>th</sup> World Zoroastrian Youth Congress</strong><br />
+            <?php renderPostalAddress()?>
+            <abbr title="Email"><i class="fa fa-envelope fa-6"></i></abbr> <?php renderEmailAddress()?>
+        </address>
     </div>
-</div>
+</section>
+
+
+<section>
+    <div class="container">
+        <h1>Social Media</h1>
+        <p>Were also on the following social media channels:</p>
+        <address class="social-media">
+            <a href="https://twitter.com/6WZYC" target="_blank"><i class="fa fa-twitter-square fa-6"></i></a>
+            <a href="https://www.facebook.com/groups/223424211030360/" target="_blank"><i class="fa fa-facebook-square fa-6"></i></a>
+            <a href="http://6wzyc.tumblr.com/" target="_blank"><i class="fa fa-tumblr-square fa-6"></i></a>
+        </address>
+    </div>
+</section>
+
+
 <?php
     
         renderFooter();
