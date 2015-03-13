44bdd3c095189486a2da4337e1ab08c31eddd270
Piran Tata <piran.tata@gmail.com>
2015-03-13 23:39:24 +1300

    Css changes to splash layout to add more sponsors

--- web/6wzyc/site/_splashLayout.php
+++ web/6wzyc/site/_splashLayout.php
@@ -18,10 +18,7 @@
         ?>
     </head>
     <body>
-        <main style="background-image:url('<?php echo createImagePath('auckland-pano.jpg',"site")?>')">
             <?php
-                
-                
                 /*
                 * Ensure that the page this layout is used for has a named function 
                 * as defined by constant PAGE_BODY (see _settings.php)
@@ -30,15 +27,5 @@
                     call_user_func(RENDER_BODY);
                 }
             ?>
-        </main>
-
-        <footer>
-            <a class="sponsor" href="http://www.seruminstitute.com/" target="_blank" rel="nofollow"><img src="<?php echo createImagePath("poona.png","sponsors")?>" alt="sponsor" /></a>
-            <img class="sponsor" src="<?php echo createImagePath("hk.png","sponsors")?>" alt="sponsor" />
-            <a class="sponsor" href="http://www.w-z-o.org/" target="_blank" rel="nofollow"><img src="<?php echo createImagePath("wzo.png","sponsors")?>" alt="sponsor" /></a>
-            <a class="sponsor" href="http://www.sailegal.biz.com.hk/" target="_blank" rel="nofollow"><img src="<?php echo createImagePath("navaz-jal-karbhari.png","sponsors")?>" alt="sponsor" /></a>
-            <img class="sponsor" src="<?php echo createImagePath("rusy-purviz-shroff.png","sponsors")?>" alt="sponsor" />
-            <img class="sponsor" src="<?php echo createImagePath("shernaz-cawas-pardiwalla.png","sponsors")?>" alt="sponsor" />
-        </footer>
     </body>
 </html>
--- web/6wzyc/site/index.php
+++ web/6wzyc/site/index.php
@@ -2,24 +2,35 @@
     require '_splashLayout.php';
     function renderBody(){
 ?>
+<main style="background-image:url('<?php echo createImagePath('auckland-pano.jpg',"site")?>')">
+    <div id="splash-label">
+        <h1><span class="site-green">6</span><span class="black">WZYC</span></h1><br />
+        <h2><span class="black">6<sup>th</sup> World Zoroastrian Youth Congress</span></h2><br />
+        <h3><span class="black">Auckland, New Zealand</span></h3><br />
+        <h3> <span class="gold">28<sup>th</sup> Dec 2015 - 2<sup>nd</sup> Jan 2016</span></h3><br />
+    </div>
 
-<div id="splash-label">
-    <h1><span class="site-green">6</span><span class="black">WZYC</span></h1><br />
-    <h2><span class="black">6<sup>th</sup> World Zoroastrian Youth Congress</span></h2><br />
-    <h3><span class="black">Auckland, New Zealand</span></h3><br />
-    <h3> <span class="gold">28<sup>th</sup> Dec 2015 - 2<sup>nd</sup> Jan 2016</span></h3><br />
+    <a id="enter-button" class="btn btn-default btn-lg" href="/about.php">
+        <span class="black">Enter Site</span>
+    </a>
 
-</div>
+</main>
 
 
-<a id="enter-button" class="btn btn-default btn-lg" href="/about.php">
-    <span class="black">Enter Site</span>
-</a>
+<footer>
+    <a class="sponsor" href="http://www.seruminstitute.com/" target="_blank" rel="nofollow"><img src="<?php echo createImagePath("poona.png","sponsors")?>" alt="sponsor" /></a>
+    <img class="sponsor" src="<?php echo createImagePath("hk.png","sponsors")?>" alt="sponsor" />
+    <a class="sponsor" href="http://www.w-z-o.org/" target="_blank" rel="nofollow"><img src="<?php echo createImagePath("wzo.png","sponsors")?>" alt="sponsor" /></a>
+    <br/>
+    <a class="sponsor" href="http://www.sailegal.biz.com.hk/" target="_blank" rel="nofollow"><img src="<?php echo createImagePath("navaz-jal-karbhari.png","sponsors")?>" alt="sponsor" /></a>
+    <img class="sponsor" src="<?php echo createImagePath("rusy-purviz-shroff.png","sponsors")?>" alt="sponsor" />
+    <img class="sponsor" src="<?php echo createImagePath("shernaz-cawas-pardiwalla.png","sponsors")?>" alt="sponsor" />
+</footer>
 
 
 <?php
     
     } 
-       
+    
 ?>
 
