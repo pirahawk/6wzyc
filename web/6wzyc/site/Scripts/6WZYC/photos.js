d4cf7f660482a6701eed53613f0e6434cfa734d5
Piran Tata <Piran Tata>
2016-08-29 18:38:14 +0100

    img library progress

--- web/6wzyc/site/Scripts/6WZYC/photos.js
+++ web/6wzyc/site/Scripts/6WZYC/photos.js
@@ -4,25 +4,38 @@ var WZYC = WZYC || {};
     if (WZYC.Images === undefined) {
         return;
     }
+    var BLOB_PATH = 'http://test6wzyc.blob.core.windows.net/images/';
 
-    var Image = function (id) {
+    var Image = function (id, label) {
         this.id = id;
-    }
-
-    var ImageLibrary = function (label, images) {
         this.label = label;
-        this.images = images;
-        this.isSelected = false;
     }
 
-    var createImageModel = function (imageId) {
-        return new Image(imageId);
+    var ImageLibrary = function (label) {
+        this.label = label;
+        this.images = [];
+        //this._isSelected = false;
+    }
+
+    Image.prototype.getUrl = function (imgType) {
+        var _self = this;
+        var seperator = '/';
+        var url = BLOB_PATH + imgType + seperator + _self.label + '/' + _self.id + '.jpg';
+        return url;
+    };
+
+    var createImageModel = function (imageId, imageFolder) {
+        return new Image(imageId, imageFolder);
     };
 
     var createImageLibraryModel = function (imageFolder) {
-        var allImages = _.map(imageFolder.Files, createImageModel);
-        var imgLibrary = new ImageLibrary(imageFolder.Folder, allImages);
-        return imgLibrary;
+        var library = new ImageLibrary(imageFolder.Folder);
+        var allImages = _.map(imageFolder.Files, function (fileId) {
+            return createImageModel(fileId, imageFolder.Folder);
+        });
+
+        library.images = allImages;
+        return library;
     };
 
     var createModel = function (folders) {
@@ -46,24 +59,145 @@ var WZYC = WZYC || {};
                 currentSelection.isSelected(false);
             }
         }
+
+
     };
 
-    ImageLibrary.prototype.asMenuItemModel = function (menu) {
+    ImageLibrary.prototype.asMenuItemModel = function (menuModel) {
         var _self = this;
         _self.displayName = ko.observable(_self.label);
-        _self.isSelected = ko.observable(_self.isSelected);
+        _self.isSelected = ko.observable(false);
 
         _self.onSelected = function () {
             _self.isSelected(true);
-            menu.notifySelectionChanged(_self);
+            menuModel.notifySelectionChanged(_self);
         };
 
         return _self;
     };
 
+
+    var ImageGalleryModel = function (menuModel, allFolders) {
+        var _self = this;
+        var imagesPerPage = 50;
+        _self.currentLibrary = ko.computed(function () {
+
+            return _.find(allFolders, function (f) {
+                return f.isSelected();
+            });
+        }, _self);
+
+        _self.currentPage = ko.observable(0);
+        _self.debugInfo = ko.computed(function () {
+            return ' page: ' + _self.currentPage();
+        }, this);
+
+        _self.canShowPrevious = ko.computed(function () {
+            return _self.currentPage() > 0;
+        }, _self);
+
+        _self.calculateTotalNumberOfPages = function () {
+            var currentLibrary = _self.currentLibrary();
+            if (!currentLibrary) {
+                return;
+            }
+            var numImages = currentLibrary.images.length;
+            return Math.ceil((numImages / imagesPerPage));
+        };
+
+        _self.calculateHighestPageNumber = function () {
+            return _self.calculateTotalNumberOfPages() - 1;
+        };
+
+        _self.canShowNext = ko.computed(function () {
+            var highestPageNumber = _self.calculateHighestPageNumber();
+            return _self.currentPage() < highestPageNumber;
+        }, _self);
+
+        _self.thumbNails = ko.computed(function () {
+            var currentLibrary = _self.currentLibrary();
+            if (!currentLibrary) {
+                return;
+            }
+            var numToSkip = _self.currentPage() * imagesPerPage;
+            var imagesToDisplay = _.take(_.drop(currentLibrary.images, numToSkip), imagesPerPage);
+
+            return _.map(imagesToDisplay, function (i) {
+                
+                var projection = {
+                    id: i.id
+                    ,
+                    thumbnail: i.getUrl('thumbnail')
+                    ,
+                   preview: i.getUrl('preview')
+                   ,
+                   full: i.getUrl('full')
+                };
+                
+                return projection;
+            });
+        }, this);
+
+        _self.currentLibrary.subscribe(function () {
+            _self.currentPage(0);
+            _self.previewImage(null);
+        });
+
+        _self.showPreviousPage = function () {
+            var currentValue = _self.currentPage();
+            if (currentValue === 0) {
+                return;
+            }
+            _self.currentPage(currentValue - 1);
+        };
+
+        _self.showNextPage = function () {
+            var currentValue = _self.currentPage();
+            var highestPageNumber = _self.calculateHighestPageNumber();
+            if (currentValue === highestPageNumber) {
+                return;
+            }
+
+            _self.currentPage(currentValue + 1);
+        };
+
+        _self.previewImage = ko.observable();
+
+        _self.shouldShowPreview = ko.computed(function(){
+            var img = _self.previewImage();
+            var css = (img || img) ? "": "hide-component" ;
+            return css;
+        },_self);
+
+        _self.shouldShowGallery = ko.computed(function(){
+            var img = _self.previewImage();
+            var css = !( img || img ) ? "": "hide-component" ;
+            return css;
+        },_self);
+
+        _self.onThumbnailSelected = function(e){
+            var selectedImage = e;
+            _self.previewImage(selectedImage);
+        }
+
+        _self.closePreview = function(){
+            var img = _self.previewImage();
+            _self.previewImage(null);
+            window.location.hash = img.id;
+        }
+        
+    }
+
     var allFolders = createModel(WZYC.Images.Folders);
-    var imgMenuElem = document.getElementById('image-menu');
     var menuModel = new MenuModel(allFolders);
+    var imageGalleryModel = new ImageGalleryModel(menuModel, allFolders);
+    allFolders[0].isSelected(true);
+
+    var imgMenuElem = document.getElementById('image-menu');
+    var imgGalleryElem = document.getElementById('image-gallery');
+
+
     ko.applyBindings(menuModel, imgMenuElem);
+    ko.applyBindings(imageGalleryModel, imgGalleryElem);
 
 })();   
--- web/6wzyc/site/photos.php
+++ web/6wzyc/site/photos.php
@@ -1,38 +1,72 @@
 <?php
-    require '_layout.php';
-    
-    function renderBody(){
-?>
+require '_layout.php';
 
-<div class="page-title">
+function renderBody(){
+    ?>
+
+  <div class="page-title">
     <h1>Congress Photos</h1>
-</div>
+  </div>
 
 
-	<section>   
-	<div class="container"> 
-		<p>All images have been seperated by the day on which they were taken</p>
-		<div id="image-menu">
-			<div data-bind="foreach: menuItems">
-				<a class="btn btn-lg" data-bind="text: displayName, click: onSelected, css: { 'btn-default': !isSelected(), 'btn-success': isSelected()}"></a>
-			</div>
+  <section>
+    <div class="container">
+      <p>All images have been seperated by the day on which they were taken</p>
+      <div id="image-menu">
+        <div data-bind="foreach: menuItems">
+          <a class="btn btn-lg" data-bind="text: displayName, click: onSelected, css: { 'btn-default': !isSelected(), 'btn-success': isSelected()}"></a>
+        </div>
 
-		</div>
-	</div>
-	</section>
-
-    <section>
-    <div id="image-gallery">
+      </div>
     </div>
-    </section>
+  </section>
 
-<?php
-        renderFooter();
-        renderScripts(
-                "lodash.min.js",
-                "knockout-3.1.0.js",
-                "6WZYC/allImages.js",
-                "6WZYC/photos.js");
-    }
-    
+  <section>
+    <div class="container" id="image-gallery">
+      <p data-bind="text: debugInfo"></p>
+
+      <div id="thumb-gallery" data-bind="css:shouldShowGallery">
+
+        <nav aria-label="...">
+          <ul class="pager">
+            <li><a href="#" data-bind="visible: canShowPrevious, click: showPreviousPage">Previous</a></li>
+            <li><a href="#" data-bind="visible: canShowNext, click: showNextPage">Next</a></li>
+          </ul>
+        </nav>
+
+        <div data-bind="foreach: thumbNails()">
+          <div class="thumbnail-container" data-bind="attr: {id: $data.id}">
+            <img class="photo-thumbnail" data-bind="attr: { src: $data.thumbnail}, click: function(){$root.onThumbnailSelected($data)}"></img>
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <div id="preview-gallery" data-bind="css:shouldShowPreview">
+        <div data-bind="with: previewImage">
+          <img data-bind="attr: {src: preview}"></img>
+          
+          <a class="btn btn-lg btn-primary" data-bind="attr: {href: full}" target="_blank">Download (high resolution)</a>
+          <a class="btn btn-lg btn-danger" data-bind="click:$root.closePreview">Close</a>
+        </div>
+      </div>
+
+
+    </div>
+
+
+
+  </section>
+
+  <?php
+    renderFooter();
+    renderScripts(
+    "lodash.min.js",
+    "knockout-3.1.0.js",
+    "6WZYC/allImages.js",
+    "6WZYC/photos.js");
+}
+
 ?>
